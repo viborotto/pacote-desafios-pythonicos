18bd5ef71a77565315666ff4a527e9b4c05f6bcb
viborotto <vittoriaborotto@gmail.com>
2020-08-08 11:23:39 -0300

    4 OK

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -14,7 +14,8 @@ Assuma que a e b tem tamanho 2 ou maior.
 
 def mix_up(a, b):
     # +++ SUA SOLUÇÃO +++
-    return
+    if len(a) and len(b) > 2:
+        return f'{a.replace(a[:2], b[:2])} {b.replace(b[:2], a[:2])}'
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
