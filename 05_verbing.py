127eb88a364e7b0f613769fd9713cec4e7be31b2
viborotto <vittoriaborotto@gmail.com>
2020-08-08 11:30:11 -0300

    5 OK

--- 05_verbing.py
+++ 05_verbing.py
@@ -12,7 +12,12 @@ Retorne o resultado da string.
 
 def verbing(s):
     # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) >= 3:
+        if s[-3:] == 'ing':
+            return f'{s}ly'
+        else:
+            return f'{s}ing'
+    return s
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
